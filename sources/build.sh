3185d7c365bd505480ff44bd8a0514ae61e0d059
Saber Rastikerdar <saber.rastikerdar@gmail.com>
2022-04-04 10:42:53 +0430

    Remove build.sh

--- sources/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bashSCRIPTS_DIR=$(dirname "$0")/../scripts
-set -euo pipefail
-
-REPO_DIR=$(dirname "$0")/..
-OUTPUT_DIR="${REPO_DIR}"
-bash "${SCRIPTS_DIR}"/make-fonts.sh --no-misc --no-css --repo-dir="${REPO_DIR}" --output-dir="${OUTPUT_DIR}"
