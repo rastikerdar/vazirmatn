72e0e06361d6a926c35e36a493814d5e03152a4d
GitHub Action <action@github.com>
2023-05-01 07:54:53 +0000

    deploy

---
+++ _next/static/chunks/pages/[lang]-a8b626f1d78538b2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[484],{9209:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/[lang]",function(){return u(4895)}])}},function(n){n.O(0,[775,204,4,895,774,888,179],(function(){return _=9209,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
---
+++ _next/static/wR7t9_LanXMJBpkIhQ7Lz/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[lang]\u002Fdocs","\u002F[lang]\u002Fdocs\u002F[slug]","\u002F[lang]","\u002F[lang]\u002Flab"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
